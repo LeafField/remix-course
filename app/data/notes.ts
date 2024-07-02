6670a8b2a99d63f85109d174ce76826fce918f59
LeafField <109466326+LeafField@users.noreply.github.com>
2024-07-02 22:15:05 +0900

    セクション2の23まで完了

--- app/data/notes.ts
+++ app/data/notes.ts
@@ -1,20 +1,22 @@
 import fs from "fs/promises";
 
-export async function getStoredNotes() {
+export async function getStoredNotes(): Promise<Notes["notes"]> {
   const rawFileContent = await fs.readFile("notes.json", { encoding: "utf-8" });
   const data: Notes = JSON.parse(rawFileContent);
   const storedNotes = data.notes ?? [];
   return storedNotes;
 }
 
-export function storeNotes(notes: Notes) {
+export function storeNotes(notes: Notes["notes"]) {
   return fs.writeFile("notes.json", JSON.stringify({ notes: notes || [] }));
 }
 
-type Notes = {
+export type Notes = {
   notes: {
     id: string;
     title: string;
     content: string;
   }[];
 };
+
+export type Note = Notes["notes"][number];
--- app/routes/notes.tsx
+++ app/routes/notes.tsx
@@ -1,22 +1,38 @@
-import { ActionFunction, LinksFunction, LoaderFunction } from "@remix-run/node";
+import { ActionFunction, LinksFunction, redirect } from "@remix-run/node";
 import NewNote, { links as newNoteLinks } from "../components/NewNote";
-import { getStoredNotes } from "../data/notes";
+import NoteList, { links as noteListLinks } from "../components/NoteList";
+import { getStoredNotes, storeNotes } from "../data/notes";
+import type { Note } from "../data/notes";
+import { useLoaderData } from "@remix-run/react";
 
 const NotePage = () => {
+  const notes = useLoaderData<typeof loader>();
   return (
     <main>
       <NewNote />
+      <NoteList notes={notes} />
     </main>
   );
 };
 
 export default NotePage;
 
+export const loader = async () => {
+  const notes = await getStoredNotes();
+  return notes;
+};
+
 export const action: ActionFunction = async ({ request }) => {
   const formData = await request.formData();
   const noteData = Object.fromEntries(formData.entries());
-  const existingNote = await getStoredNotes();
   noteData.id = new Date().toISOString();
+  const existingNote = await getStoredNotes();
+  const updateNotes = existingNote.concat(noteData as Note);
+  await storeNotes(updateNotes);
+  return redirect("/notes");
 };
 
-export const links: LinksFunction = () => [...newNoteLinks()];
+export const links: LinksFunction = () => [
+  ...newNoteLinks(),
+  ...noteListLinks(),
+];
